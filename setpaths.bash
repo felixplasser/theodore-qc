1d67564aaaf39201c67098d03c7699b5321ee5f6
Felix (cc8) <felix.plasser@iwr.uni-heidelberg.de>
2015-02-16 12:36:57 +0100

    use HOME variable

--- setpaths.bash
+++ setpaths.bash
@@ -1,9 +1,9 @@
 #!/bin/bash
 # source this file to set the paths
 
-export THEODIR=/user/plasserf/programs/TheoDORE/GIT
+export THEODIR=$HOME/programs/TheoDORE/GIT
 export PATH=$THEODIR/bin:$PATH
 export PYTHONPATH=$THEODIR/lib
 
 # add external packages here if they have not been installed in the default locations
-export PYTHONPATH=/export/home/fplasser/programs/cclib/cclib-1.3/build/lib.linux-x86_64-2.7:$PYTHONPATH
+export PYTHONPATH=$HOME/programs/cclib/cclib-1.3/build/lib.linux-x86_64-2.7:$PYTHONPATH
