246c7dacc06da2207c0ecb297127b2e1904b0945
Felix Plasser <felix.plasser@univie.ac.at>
2017-04-25 16:47:55 +0200

    add script for tden overlap

---
+++ bin/tden_OV.py
@@ -0,0 +1,108 @@
+#!/usr/bin/python
+"""
+Compute the overlap between transition density matrices.
+"""
+
+import theo_header, lib_tden, input_options, error_handler
+import numpy
+import sys, os
+
+def ihelp():
+    print " tden_OV.py <dir1> <dir2> [<AO_OV>]"
+    print " Command line options:"
+    print "  -h, -H, -help: print this help"
+    print "  -ifile, -f [dens_ana.in]: name of the input file"
+    exit(0)
+
+ifile = 'tden_OV.in'
+
+largs = []
+arg=sys.argv.pop(0)
+while len(sys.argv)>0:
+    arg = sys.argv.pop(0)
+    if arg in ["-h", "-H", "-help"]:
+        ihelp()
+    elif arg == '-ifile' or arg == '-f':
+        ifile = sys.argv.pop(0)
+    elif arg[0] == '-':
+        raise error_handler.ElseError(arg, 'command line option')
+    else:
+        largs.append(arg)
+
+if len(largs) == 2:
+    dir1 = largs[0]
+    dir2 = largs[1]
+    AO_OV = None
+elif len(largs) == 3:
+    dir1  = largs[0]
+    dir2  = largs[1]
+    AO_OV = largs[2]
+else:
+    ihelp()
+
+if not os.path.exists(ifile):
+    print 'Input file %s not found!'%ifile
+    print 'Please create this file using theoinp or specify its location using -ifile\n'
+    ihelp()
+
+ioptions = input_options.tden_ana_options(ifile)
+theo_header.print_header('Transition density matrix overlap', ioptions=ioptions)
+
+sdir = os.getcwd()
+
+# Read info for the first job
+os.chdir(dir1)
+tdena1 = lib_tden.tden_ana(ioptions)
+if 'mo_file' in ioptions:
+    tdena1.read_mos()
+tdena1.read_dens()
+os.chdir(sdir)
+
+# Read info for the second job
+os.chdir(dir2)
+tdena2 = lib_tden.tden_ana(ioptions)
+if 'mo_file' in ioptions:
+    tdena2.read_mos()
+tdena2.read_dens()
+os.chdir(sdir)
+
+if AO_OV == None:
+    print "Constructing AO-overlap matrix from MO-coefficients"
+    tdena1.mos.compute_inverse()
+    SMO = numpy.dot(tdena1.mos.inv_mo_mat, tdena2.mos.mo_mat)
+    if ioptions['lvprt'] >= 2:
+        SAO = numpy.dot(tdena1.mos.inv_mo_mat.T, tdena1.mos.inv_mo_mat)
+else:
+    SAO  = numpy.array([[float(s) for s in line.split()] for line in open(AO_OV, 'r').readlines()[1:]])
+
+    CS  = numpy.dot(tdena1.mos.mo_mat.T, SAO)
+    SMO = numpy.dot(CS, tdena2.mos.mo_mat)
+
+if ioptions['lvprt'] >= 2:
+    print "AO-overlap matrix:"
+    print SAO
+    print
+
+    print "MO-overlap matrix:"
+    print SMO
+    print
+
+print "        ",
+for state2 in tdena2.state_list:
+    print "   |%7s>"%state2['name'],
+print
+
+for state1 in tdena1.state_list:
+    print "<%7s|"%state1['name'],
+    tden1 = state1['tden']
+    DS1 = numpy.dot(tden1, SMO)
+    mdim = tden1.shape[0]
+    SDS1 = numpy.dot(SMO[:mdim,:mdim], DS1)
+    for state2 in tdena2.state_list:
+        if state1['mult'] == state2['mult']:
+            tden2 = state2['tden']
+            OV = sum((SDS1 * tden2).flatten())
+            print " % .8f"%OV,
+        else:
+            print " % .1f       "%0.,
+    print
