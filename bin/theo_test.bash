0f2711546f44af28fc1f482451b7551892278561
Felix Plasser <felix.plasser@univie.ac.at>
2018-01-05 17:12:41 +0100

    Molcas/libwfa Test, #50

--- bin/theo_test.bash
+++ bin/theo_test.bash
@@ -13,7 +13,7 @@ if [ -z "$THEODIR" ]; then
    exit 1
 fi
 
-stddirs="pyrrole.qcadc hexatriene.colmrci fa2.ricc2 pv2p.escf pv2p.qctddft pyridine.ricc2 fa2.col fa2.rassi fa2.terachem tyrosine.ricc2-es2es biphenyl.tddftb"
+stddirs="pyrrole.qcadc hexatriene.colmrci fa2.ricc2 pv2p.escf pv2p.qctddft pyridine.ricc2 fa2.col fa2.rassi fa2.rassi-libwfa fa2.terachem tyrosine.ricc2-es2es biphenyl.tddftb"
 obdirs="ir_c3n3.qctddft"
 cclibdirs="fa2.cclib SnH4-ecp.firefly"
 adfdirs="fa2.adf"
