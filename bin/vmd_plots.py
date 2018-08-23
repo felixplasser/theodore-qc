ce9b476f37d37b619894f33fff858e1a13c6db27
Felix Plasser <F.Plasser@lboro.ac.uk>
2018-08-23 14:31:35 +0100

    Volume integrals for vmd_plots.py

--- bin/vmd_plots.py
+++ bin/vmd_plots.py
@@ -3,24 +3,37 @@
 Automatic plotting of densities or MOs with vmd.
 """
 
-import error_handler, theo_header, input_options, lib_struc, lib_file
+import error_handler, theo_header, input_options, lib_struc, lib_file, lib_util
 
 class vmd_options(input_options.write_options):
     def vmd_input(self):
+        self.read_yn('Compute volume integrals over cube files for isovalues?', 'do_vol', False)
         self.read_yn('Use special DNTO mode?', 'dnto', False)
         if self['dnto']:
             self['niso'] = 2
-            self.read_float('Isovalue for conditional density', 'iso1', 0.003)
+            if not self['do_vol']:
+                self.read_float('Isovalue for conditional density', 'iso1', 0.003)
+                self.read_float('Isovalue for probe density', 'iso2', self['iso1'])
+            else:
+                self.read_float('Volume integral for conditional density', 'iso1', 0.75)
+                self.read_float('Volume integral for probe density', 'iso2', self['iso1'])
             self.read_str('VMD Material for conditional density', 'mat1', 'AOShiny')
-            self.read_float('Isovalue for probe density', 'iso2', self['iso1'])
             self.read_str('VMD Material for probe density', 'mat2', 'Glass1')
         else:
             self.read_int('How many isovalues (1 or 2)?', 'niso', 2)
-            self.read_float('First isovalue', 'iso1', 0.003)
+            if not self['do_vol']:
+                self.read_float('First isovalue', 'iso1', 0.003)
+            else:
+                self.read_float('Volume integral for first density', 'iso1', 0.5)
             self.read_str('VMD Material for first density', 'mat1', 'AOShiny')
             if self['niso'] >= 2:
-                self.read_float('Second isovalue', 'iso2', self['iso1']/3.)
+                if not self['do_vol']:
+                    self.read_float('Second isovalue', 'iso2', self['iso1']/3.)
+                else:
+                    self.read_float('Volume integral for second density', 'iso2', 0.9)
                 self.read_str('VMD Material for second density', 'mat2', 'Glass3')
+            else:
+                self['iso2'] = self['iso1']/3.
 
         self.read_int('Width of images in output html file', 'width', 400)
         self.read_int('Number of columns in the output html file', 'ncol', 4)
@@ -70,6 +83,8 @@ mol modstyle 0 0 Licorice 0.100000 30.000000 30.000000
 # material change ambient Ghost 0.300000
 # material change opacity Ghost 0.100000
 # material change shininess Ghost 0.000000
+
+        iso1 = 0.001 if self['do_vol'] else self['iso1']
         lf.write("""mol addrep 0
 mol addrep 0
 mol modmaterial 1 0 %s
@@ -78,9 +93,10 @@ mol modstyle 1 0 Isosurface  %.5f 0 0 0 1 1
 mol modstyle 2 0 Isosurface -%.5f 0 0 0 1 1
 mol modcolor 1 0 ColorID 0
 mol modcolor 2 0 ColorID 1
-"""%(self['mat1'], self['mat1'], self['iso1'], self['iso1']))
+"""%(self['mat1'], self['mat1'], iso1, iso1))
 
         if self['niso'] >= 2:
+            iso2 = 0.001 if self['do_vol'] else self['iso2']
             lf.write("""mol addrep 0
 mol addrep 0
 mol modmaterial 3 0 %s
@@ -89,7 +105,7 @@ mol modstyle 3 0 Isosurface  %.5f 0 0 0 1 1
 mol modstyle 4 0 Isosurface -%.5f 0 0 0 1 1
 mol modcolor 3 0 ColorID 0
 mol modcolor 4 0 ColorID 1
-"""%(self['mat2'], self['mat2'], self['iso2'], self['iso2']))
+"""%(self['mat2'], self['mat2'], iso2, iso2))
 
         struc = lib_struc.structure()
         for pltf in pltfiles:
@@ -102,20 +118,26 @@ mol modcolor 4 0 ColorID 1
         lf.close()
         print "File %s written."%lf.name
 
-    def write_pfile(self, pltfiles):
+    def write_pfile(self, pltfiles, auxfiles=[]):
         """
         File used for plotting.
         """
+        iso1 = self['iso1']
+        iso2 = self['iso2']
         pf = open(self['pfile'], 'w')
         for iplt, pltf in enumerate(pltfiles):
-            pf.write("mol modstyle 1 0 Isosurface  %.5f %i 0 0 1 1\n"%(self['iso1'], iplt))
-            pf.write("mol modstyle 2 0 Isosurface -%.5f %i 0 0 1 1\n"%(self['iso1'], iplt))
+            if self['do_vol']:
+                iso1, iso2 = lib_util.cube_file(pltf).ret_isovals([self['iso1'], self['iso2']])
+            pf.write("mol modstyle 1 0 Isosurface  %.5f %i 0 0 1 1\n"%(iso1, iplt))
+            pf.write("mol modstyle 2 0 Isosurface -%.5f %i 0 0 1 1\n"%(iso1, iplt))
             if self['dnto']:
-                pf.write("mol modstyle 3 0 Isosurface  %.5f %i 0 0 1 1\n"%(self['iso2'], iplt + len(pltfiles)))
-                pf.write("mol modstyle 4 0 Isosurface -%.5f %i 0 0 1 1\n"%(self['iso2'], iplt + len(pltfiles)))
+                if self['do_vol']:
+                    iso2 = lib_util.cube_file(auxfiles[iplt]).ret_isovals([self['iso2']])[0]
+                pf.write("mol modstyle 3 0 Isosurface  %.5f %i 0 0 1 1\n"%(iso2, iplt + len(pltfiles)))
+                pf.write("mol modstyle 4 0 Isosurface -%.5f %i 0 0 1 1\n"%(iso2, iplt + len(pltfiles)))
             elif self['niso'] >= 2:
-                pf.write("mol modstyle 3 0 Isosurface  %.5f %i 0 0 1 1\n"%(self['iso2'], iplt))
-                pf.write("mol modstyle 4 0 Isosurface -%.5f %i 0 0 1 1\n"%(self['iso2'], iplt))
+                pf.write("mol modstyle 3 0 Isosurface  %.5f %i 0 0 1 1\n"%(iso2, iplt))
+                pf.write("mol modstyle 4 0 Isosurface -%.5f %i 0 0 1 1\n"%(iso2, iplt))
             pf.write("render TachyonInternal %s.tga\n"%pltf)
 
         pf.close()
@@ -172,7 +194,7 @@ def run():
         pltfiles, auxfiles = vopt.mod_pltfiles(pltfiles)
 
     vopt.write_lfile(pltfiles, auxfiles)
-    vopt.write_pfile(pltfiles)
+    vopt.write_pfile(pltfiles, auxfiles)
     vopt.write_cfile(pltfiles)
     vopt.write_hfile(pltfiles)
 
