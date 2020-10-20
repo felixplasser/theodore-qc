109ada13f8de9c1748440ffea9ddafef9fe47fea
Felix Plasser (lin) <f.plasser@lboro.ac.uk>
2020-10-20 10:00:44 +0100

    Orca: parse length repr. of osc. strengths
    reported by Sebastian Mai

--- bin/theo_test.bash
+++ bin/theo_test.bash
@@ -86,7 +86,7 @@ do
     for rfile in `ls "$sdir/REF_FILES"`
     do
         echo "  -> $rfile"
-        diff -w -I 'TheoDORE\|python-openbabel\|emulation\|wall time\|Contributions|not found\|' "$sdir/REF_FILES/$rfile" $rfile
+        diff -w -I 'TheoDORE\|python-openbabel\|emulation\|wall time\|Contributions|not found|' "$sdir/REF_FILES/$rfile" $rfile
         chk=$((chk+$?))
     done
 
