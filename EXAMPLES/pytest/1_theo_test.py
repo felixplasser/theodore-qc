2e6f110ca8a8288a4362590e7a5e189034dcf26e
Felix Plasser (lin) <f.plasser@lboro.ac.uk>
2021-04-15 14:13:12 +0100

    Crude version of diff is running

--- EXAMPLES/pytest/1_theo_test.py
+++ EXAMPLES/pytest/1_theo_test.py
@@ -7,7 +7,7 @@ Activate this by running pytest [-v/-s] in the EXAMPLES directory.
 
 # TODO: Parts of this can be moved into its own library later
 
-import os, shutil
+import os, shutil, sys, subprocess, difflib
 
 stddirs="pyrrole.qcadc hexatriene.colmrci fa2.ricc2 pv2p.escf pv2p.qctddft pyridine.ricc2 fa2.col fa2.rassi fa2.rassi-libwfa fa2.terachem fa2.dftmrci fa2.cation tyrosine.ricc2-es2es biphenyl.tddftb naphth.fchk water.ricc2"
 obdirs="ir_c3n3.qctddft"
@@ -21,15 +21,19 @@ def test_header():
     print
     #theo_header.print_header('TheoDORE tests')
     print("THEODIR: ", os.environ["THEODIR"])
+    sys.path.append(os.environ["THEODIR"] + "/bin")
 
 def run(rdirs):
     print
     for rdir in rdirs.split():
-        print(" *** Running ", rdir)
+        print(" *** Starting test %s ..."%rdir)
         tjob(rdir).run_standard()
 
 def test_cclib():
-    run("fa2.cclib SnH4-ecp.firefly H2S.orca")
+    run(cclibdirs)
+
+def test_standard():
+    run(stddirs)
 
 ###
 
@@ -43,6 +47,18 @@ class tjob:
             shutil.rmtree('RUN')
         shutil.copytree('QC_FILES', 'RUN')
 
+    def check(self):
+        os.chdir(self.path + '/RUN')
+        print("Checking primary output files")
+        for rfile in os.listdir('../REF_FILES'):
+            print("  -> " + rfile)
+            ref = open('../REF_FILES/'+rfile).readlines()
+            run = open(rfile).readlines()
+            diffl = list(difflib.unified_diff(ref, run, fromfile='Reference', tofile='Current run'))
+            for line in diffl:
+                print(line, end="")
+            assert(len(diffl) == 0)
+
     def run_standard(self):
         """
         Run tests in standard format.
@@ -52,6 +68,15 @@ class tjob:
         os.chdir(self.path + '/RUN')
         for ifile in os.listdir('../IN_FILES'):
             print(ifile)
-            shutil.copy("../IN_FILES/"+ifile, '.')
+            shutil.copy("../IN_FILES/"+ifile, 'dens_ana.in')
             
             (dtype, dummy, atype) = ifile.split('.')
+            comm="analyze_%s.py"%dtype
+            
+            with open("analyze_%s.out"%atype, 'w') as outf:
+                subprocess.run(comm, stdout=outf)
+            # if dtype == 'tden':
+            #     sys.argv = ['analyze_tden.py']
+            #     import analyze_tden
+
+        self.check()
