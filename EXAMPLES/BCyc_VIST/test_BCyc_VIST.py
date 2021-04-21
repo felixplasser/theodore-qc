34090134273fbbe87e10e5884dd0ecea7da4d5ae
Felix Plasser <F.Plasser@lboro.ac.uk>
2021-04-21 15:34:36 +0100

    VIST tests running with pytest

---
+++ EXAMPLES/BCyc_VIST/test_BCyc_VIST.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+import os, subprocess
+from theodore import lib_pytest
+
+def test_BCyc_VIST():
+    pjob = lib_pytest.pytest_job("BCyc_VIST")
+
+    with open("run_simple.out", 'w') as outf:
+        comm = ['plot_VIST.py', '-p', '-o', 'simple.vmd', 'neutral.log']
+        subprocess.check_call(comm, stdout=outf)
+
+    with open("run_VIST.out", 'w') as outf:
+        comm = ['plot_VIST.py', '-c', '-v', '0 4', 'neutral.log', 'triplet.log', '2M.log']
+        subprocess.check_call(comm, stdout=outf)
+
+    pjob.check()
