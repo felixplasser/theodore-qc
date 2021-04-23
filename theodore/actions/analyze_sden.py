40c542a2dc799ac9d9fb874ed738c1bbc1728ec4
Maximilian Menger <menger.maximilian@gmail.com>
2021-04-23 03:41:30 +0200

    created basic actions for sden and tden

--- theodore/actions/analyze_sden.py
+++ theodore/actions/analyze_sden.py
@@ -1,40 +1,42 @@
-"""
-Driver script for analyzing a set of NO files.
-"""
-
-from __future__ import print_function, division
-import os, sys
-from .. import theo_header, lib_sden, input_options
-from .theotools import get_ifile_commandline
-import argparse
+from .actions import Action
+from .theotools import timeit
+from .. import theo_header, lib_sden, lib_exciton, input_options
 
 
-def analyze_sden():
-    # header
-    theo_header.print_header('State density matrix analysis')
-    ifile = get_ifile_commandline()
-    #
-    ioptions = input_options.sden_ana_options(ifile)
+class AnalyzeSden(Action):
 
-    #--------------------------------------------------------------------------#
-    # Parsing and computations
-    #--------------------------------------------------------------------------#
+    name = 'analyze_sden'
 
-    sdena = lib_sden.sden_ana(ioptions)
-    if 'mo_file' in ioptions: sdena.read_mos()
-    sdena.read_dens()
+    _questions = """
+    ifile = dens_ana.in :: file
+    """
 
-    if ioptions['NO_ana']:  sdena.compute_all_NO()
-    if ioptions['AD_ana']:  sdena.compute_all_AD()
-    if ioptions['BO_ana']:  sdena.compute_all_BO()
+    @timeit
+    def run(ifile):
+        # header
+        theo_header.print_header('State density matrix analysis')
+        #
+        ioptions = input_options.sden_ana_options(ifile)
 
-    if ioptions['comp_rho']: sdena.compute_rho()
+        #--------------------------------------------------------------------------#
+        # Parsing and computations
+        #--------------------------------------------------------------------------#
 
-    #--------------------------------------------------------------------------#
-    # Print out
-    #--------------------------------------------------------------------------#
-    if ioptions['pop_ana']: sdena.print_all_pop_table()
-    if ioptions['BO_ana']:  sdena.print_all_BO()
-    if ioptions['mo_pop_type'] > 0: sdena.print_mo_pops(ioptions['mo_pop_type'])
+        sdena = lib_sden.sden_ana(ioptions)
+        if 'mo_file' in ioptions: sdena.read_mos()
+        sdena.read_dens()
 
-    sdena.print_summary()
+        if ioptions['NO_ana']:  sdena.compute_all_NO()
+        if ioptions['AD_ana']:  sdena.compute_all_AD()
+        if ioptions['BO_ana']:  sdena.compute_all_BO()
+
+        if ioptions['comp_rho']: sdena.compute_rho()
+
+        #--------------------------------------------------------------------------#
+        # Print out
+        #--------------------------------------------------------------------------#
+        if ioptions['pop_ana']: sdena.print_all_pop_table()
+        if ioptions['BO_ana']:  sdena.print_all_BO()
+        if ioptions['mo_pop_type'] > 0: sdena.print_mo_pops(ioptions['mo_pop_type'])
+
+        sdena.print_summary()
--- theodore/actions/analyze_tden.py
+++ theodore/actions/analyze_tden.py
@@ -1,60 +1,55 @@
-"""
-Driver script for transition density matrix analysis.
-"""
-from __future__ import print_function, division
-import os, sys, time
-from .theotools import get_ifile_commandline, timeit
-
-from .. import theo_header, lib_tden, lib_exciton, input_options, error_handler
+from .actions import Action
+from .theotools import timeit
+from .. import theo_header, lib_tden, lib_exciton, input_options
 
 
+class AnalyzeTden(Action):
 
-#--------------------------------------------------------------------------#
-# Parsing and computations
-#--------------------------------------------------------------------------#
+    name = 'analyze_tden'
 
-@timeit
-def analyze_tden():
-    #
-    ifile = get_ifile_commandline()
+    _questions = """
+    ifile = dens_ana.in :: file
+    """
 
-    ioptions = input_options.tden_ana_options(ifile)
-    theo_header.print_header('Transition density matrix analysis', ioptions=ioptions)
+    @timeit
+    def run(ifile):
+        ioptions = input_options.tden_ana_options(ifile)
+        theo_header.print_header('Transition density matrix analysis', ioptions=ioptions)
 
-    tdena = lib_tden.tden_ana(ioptions)
-    if 'mo_file' in ioptions: tdena.read_mos()
+        tdena = lib_tden.tden_ana(ioptions)
+        if 'mo_file' in ioptions: tdena.read_mos()
 
-    tdena.read_dens()
+        tdena.read_dens()
 
-    if 'at_lists' in ioptions or ioptions['eh_pop'] >= 1:
-        tdena.compute_all_OmAt()
+        if 'at_lists' in ioptions or ioptions['eh_pop'] >= 1:
+            tdena.compute_all_OmAt()
 
-    if 'at_lists' in ioptions:
-        tdena.compute_all_OmFrag()
-        if ioptions['print_OmFrag']: 
-            tdena.fprint_OmFrag()
+        if 'at_lists' in ioptions:
+            tdena.compute_all_OmFrag()
+            if ioptions['print_OmFrag']: 
+                tdena.fprint_OmFrag()
 
-    if ioptions['comp_ntos']:  
-        tdena.compute_all_NTO()
-    if ioptions['comp_dntos']: 
-        tdena.compute_all_DNTO()
-    if ioptions['comp_p_h_dens']: 
-        tdena.compute_p_h_dens()
-    if ioptions['comp_rho0n']: 
-        tdena.compute_rho_0_n()
+        if ioptions['comp_ntos']:  
+            tdena.compute_all_NTO()
+        if ioptions['comp_dntos']: 
+            tdena.compute_all_DNTO()
+        if ioptions['comp_p_h_dens']: 
+            tdena.compute_p_h_dens()
+        if ioptions['comp_rho0n']: 
+            tdena.compute_rho_0_n()
 
-    if 'RMSeh' in ioptions.get('prop_list') or 'MAeh' in ioptions.get('prop_list') or 'Eb' in ioptions.get('prop_list'):
-        exca = lib_exciton.exciton_analysis()
-        exca.get_distance_matrix(tdena.struc)
-        tdena.analyze_excitons(exca)
+        if 'RMSeh' in ioptions.get('prop_list') or 'MAeh' in ioptions.get('prop_list') or 'Eb' in ioptions.get('prop_list'):
+            exca = lib_exciton.exciton_analysis()
+            exca.get_distance_matrix(tdena.struc)
+            tdena.analyze_excitons(exca)
 
-    if 'Phe' in ioptions['prop_list']:
-        tdena.compute_all_Phe()
+        if 'Phe' in ioptions['prop_list']:
+            tdena.compute_all_Phe()
 
-    #--------------------------------------------------------------------------#
-    # Print-out
-    #--------------------------------------------------------------------------#
+        #--------------------------------------------------------------------------#
+        # Print-out
+        #--------------------------------------------------------------------------#
 
-    tdena.print_all_eh_pop()
+        tdena.print_all_eh_pop()
 
-    tdena.print_summary()
+        tdena.print_summary()
