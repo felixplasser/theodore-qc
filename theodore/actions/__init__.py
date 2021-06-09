b230b6a2ad2eace6c5ecadeec7480f16d7f52399
Maximilian Menger <menger.maximilian@gmail.com>
2021-06-09 13:31:10 +0200

    added theodore settings

--- theodore/actions/__init__.py
+++ theodore/actions/__init__.py
@@ -7,4 +7,15 @@ from .. import theo_header
 
 
 def run():
-    ActionFactory.from_commandline(description=theo_header.ret_header())
+    settings = {
+            'description': theo_header.ret_header(),
+            'logo': None,
+            'main_order': ['logo', 'description', 'pos_args', 'opt_args', 'usage', 'space'],
+            'error_order': ['logo', 'usage', 'space', 'error'],
+            'arg_format': {
+                'name': 12,
+                'comment': 40,
+                'seperator': 2,
+            },
+    }
+    ActionFactory.from_commandline(description=settings)
