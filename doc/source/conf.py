d365e166a51ab26f8917e51a05cb34892e597301
Felix Plasser (lin) <f.plasser@lboro.ac.uk>
2022-08-31 11:19:24 +0100

    update to new web page structure

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -22,7 +22,7 @@ copyright = '2021, Felix Plasser'
 author = 'Felix Plasser'
 
 # The full version, including alpha/beta/rc tags
-release = '3.0_alpha'
+release = '3.0'
 
 
 # -- General configuration ---------------------------------------------------
