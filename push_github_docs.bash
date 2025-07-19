39ab8a3a6dedc1dc71e2b45467eb12889b85d7af
Felix Plasser <f.plasser@lboro.ac.uk>
2025-07-19 19:40:42 +0100

    doc workflow

--- push_github_docs.bash
+++ push_github_docs.bash
@@ -1,17 +1,16 @@
+#!/bin/bash
 # Preparation of documentation:
 
 # Change relevant files in master branch
 
-git pull origin master
+git pull origin main || exit 1
 
 cd doc
-make html
+make html || exit 1
 cd ..
 /bin/cp -r doc/html/* docs
 /bin/cp -r doc/build/html/* docs/docs
 
 # submit docs to GIT
-git commit docs
+git commit -m 'doc update' docs
 git push origin docs2
-
-# Static upload to github
